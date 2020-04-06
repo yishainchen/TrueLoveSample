e5548e32284c40d3858a603bb7576bf7f9556ad8
yishainchen <diligent1230@hotmail.com>
2020-04-06 14:31:51 +0800

    Initial Commit

---
+++ TrueLoveSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrueLoveSample
+//
+//  Created by CHEN YI-SHAIN on 2020/4/6.
+//  Copyright © 2020 CHEN YI-SHAIN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
