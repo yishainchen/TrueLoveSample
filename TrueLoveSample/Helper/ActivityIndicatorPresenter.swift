097dea476acade14f81db632236b71107f472003
yishainchen <diligent1230@hotmail.com>
2020-04-07 09:41:28 +0800

    add activityIndicator file

---
+++ TrueLoveSample/Helper/ActivityIndicatorPresenter.swift
@@ -0,0 +1,42 @@
+//
+//  ActivityIndicatorPresenter.swift
+//  TrueLoveSample
+//
+//  Created by CHEN YI-SHAIN on 2020/4/7.
+//  Copyright © 2020 CHEN YI-SHAIN. All rights reserved.
+//
+
+import UIKit
+
+public protocol ActivityIndicatorPresenter {
+
+    /// The activity indicator
+    var activityIndicator: UIActivityIndicatorView { get }
+
+    /// Show the activity indicator in the view
+    func showActivityIndicator()
+
+    /// Hide the activity indicator in the view
+    func hideActivityIndicator()
+}
+
+public extension ActivityIndicatorPresenter where Self: UIViewController {
+
+    func showActivityIndicator() {
+        DispatchQueue.main.async {
+
+            self.activityIndicator.style = .large
+            self.activityIndicator.frame = CGRect(x: 0, y: 0, width: 80, height: 80) //or whatever size you would like
+            self.activityIndicator.center = CGPoint(x: self.view.bounds.size.width / 2, y: self.view.bounds.height / 2)
+            self.view.addSubview(self.activityIndicator)
+            self.activityIndicator.startAnimating()
+        }
+    }
+
+    func hideActivityIndicator() {
+        DispatchQueue.main.async {
+            self.activityIndicator.stopAnimating()
+            self.activityIndicator.removeFromSuperview()
+        }
+    }
+}
